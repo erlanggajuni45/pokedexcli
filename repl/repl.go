41bee3ae11927ba21faf4ac4c5d63d8c2fff7462
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-07-10 14:04:34 +0700

    run the command depend on user input on REPL

--- repl/repl.go
+++ repl/repl.go
@@ -25,6 +25,15 @@ func StartREPL() {
 		if len(inputs) == 0 {
 			continue
 		}
-		fmt.Printf("Your command was: %s\n", inputs[0])
+
+		commandName := inputs[0]
+		if command, exists := getCommands()[commandName]; exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Printf("Error occurred while executing command '%s': %v\n", command.name, err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
 }
